15319a6e6149be2c77f5ceba72c86c893a2c0856
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-08-09 23:21:23 +0545

    finishing backend with chess,js libary

--- backend/src/Game.ts
+++ backend/src/Game.ts
@@ -1,6 +1,81 @@
-interface Game {
-  id: number;
-  name: string;
-  player1: WebSocket;
-  player2: WebSocket;
+import { WebSocket } from "ws";
+import { Chess } from "chess.js";
+import { GAME_OVER, INIT_GAME, MOVE } from "./message";
+
+export class Game {
+  public player1: WebSocket;
+  public player2: WebSocket;
+  private board: Chess;
+  private moves: string[];
+  private startTime: Date;
+
+  constructor(player1: WebSocket, player2: WebSocket) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = new Chess();
+    this.moves = [];
+    this.startTime = new Date();
+    this.player1.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "white",
+        },
+      })
+    );
+    this.player2.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "black",
+        },
+      })
+    );
+  }
+  makeMove(
+    socket: WebSocket,
+    move: {
+      from: string;
+      to: string;
+    }
+  ) {
+    if (this.board.moves.length % 2 === 0 && socket !== this.player1) {
+      return;
+    }
+    if (this.board.moves.length % 2 === 0 && socket !== this.player2) {
+      return;
+    }
+    try {
+      this.board.move(move);
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+    if (this.board.isGameOver()) {
+      this.player1.emit(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+      return;
+    }
+    if (this.board.moves.length % 2 === 0) {
+      this.player2.emit(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    } else {
+      this.player1.emit(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    }
+  }
 }
