c96a6f0ea5782e7dbce7ec47c0d9edb4ecb557b7
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-08-22 14:03:51 +0545

    hooks and chess board

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,3 +1,29 @@
-export const ChessBoard = () => {
-  return <div>ChessBoard</div>;
+import { Color, PieceSymbol, Square } from "chess.js";
+
+export const ChessBoard = ({
+  board,
+}: {
+  board: ({
+    square: Square;
+    type: PieceSymbol;
+    color: Color;
+  } | null)[][];
+}) => {
+  return (
+    <div>
+      {board.map((row, i) => {
+        return (
+          <div key={i} className="">
+            {row.map((square, j) => {
+              return (
+                <div key={j} className="">
+                  {square ? square.type : ""}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,21 +1,60 @@
-import React from "react";
-import { ChessBoard } from "../components/ChessBoard";
 import { Button } from "../components/Button";
-import { useNavigate } from "react-router-dom";
+import { Chess } from "chess.js";
+import { useSocket } from "../hooks/useSocket";
+import { useEffect, useState } from "react";
+import { ChessBoard } from "../components/ChessBoard";
+
+//there is code repetation
+export const INIT_GAME = "init_game";
+export const MOVE = "move";
+export const GAME_OVER = "game_over";
 
 export const Game = () => {
-  const navigate = useNavigate();
+  const socket = useSocket();
+  const [chess, setChess] = useState(new Chess());
+  const [board, setBoard] = useState(chess.board());
+  useEffect(() => {
+    if (!socket) {
+      return;
+    }
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      console.log(message);
+      switch (message.type) {
+        case INIT_GAME:
+          setChess(new Chess());
+          setBoard(chess.board());
+          console.log("Game Started");
+          break;
+        case MOVE: {
+          const move = message.payload;
+          chess.move(move);
+          setBoard(chess.board());
+          console.log("move Made");
+          break;
+        }
+        case GAME_OVER:
+          console.log("Game over");
+          break;
+      }
+    };
+  }, [socket]);
+  if (!socket) return <div>Connecting ...</div>;
   return (
     <div className="justify-center flex">
       <div className="pt-8 max-v-screen-lg">
         <div className="grid grid-cols-1">
           <div>
-            <ChessBoard />
+            <ChessBoard board={board} />
           </div>
           <div>
             <Button
               onClick={() => {
-                navigate("/game");
+                socket.send(
+                  JSON.stringify({
+                    type: INIT_GAME,
+                  })
+                );
               }}
             >
               Play Online
